658b637bb6aa25764631defd87393d418cad3c49
JS-SeokMin <julissaanchundia9@gmail.com>
2026-05-11 00:24:33 -0500

    Structs de eventos (Julissa)

---
+++ main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
+)
+
+func main() {
+
+	// Crear routerr
+	r := chi.NewRouter()
+
+	// Ruta de prueba
+	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("API Asociación Estudiantil"))
+	})
+
+	// Iniciar servidor
+	log.Println("Servidor ejecutándose en puerto 8080")
+
+	err := http.ListenAndServe(":8080", r)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
