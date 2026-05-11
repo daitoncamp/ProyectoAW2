16183b3868c832a0f9b2c559685a5cc5aa3ec384
daitoncamp <daitonxe1@gmail.com>
2026-05-10 21:50:53 -0500

    Implement main server functionality with routing and logging

--- Proyecto_AWEBII/main.go
+++ Proyecto_AWEBII/main.go
@@ -1,1 +1,25 @@
 package main
+
+import (
+	"log"
+	"net/http"
+)
+
+func main() {
+
+	// Crear router
+	r := chi.NewRouter()
+
+	// Ruta de prueba
+	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("API Asociación Estudiantil"))
+	})
+
+	// Iniciar servidor
+	log.Println("Servidor ejecutándose en puerto 8080")
+
+	err := http.ListenAndServe(":8080", r)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
