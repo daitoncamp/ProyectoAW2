fba3e4220dac67677b81bb6da1a1b48dee99d27e
JS-SeokMin <julissaanchundia9@gmail.com>
2026-06-08 00:21:09 -0500

    Crear almacenamiento y servicios CRUD de eventos

--- internal/services/evento_service.go
+++ internal/services/evento_service.go
@@ -1,1 +1,51 @@
 package services
+
+import (
+	"Proyecto_AWEBII/internal/models"
+	"Proyecto_AWEBII/internal/storage"
+	"errors"
+)
+
+func ObtenerEventos() []models.Evento {
+	return storage.Eventos
+}
+func CrearEvento(evento models.Evento) {
+	storage.Eventos = append(storage.Eventos, evento)
+}
+
+func ObtenerEventoPorID(id int) (models.Evento, error) {
+	for _, evento := range storage.Eventos {
+		if evento.ID == id {
+			return evento, nil
+		}
+	}
+	return models.Evento{}, errors.New("evento no encontrado")
+}
+
+func ActualizarEvento(id int, eventoActualizado models.Evento) error {
+	for i, evento := range storage.Eventos {
+		if evento.ID == id {
+			storage.Eventos[i] = eventoActualizado
+			return nil
+		}
+	}
+	return errors.New("evento no encontrado")
+}
+
+func EliminarEvento(id int) error {
+	for i, evento := range storage.Eventos {
+		if evento.ID == id {
+			storage.Eventos = append(storage.Eventos[:i], storage.Eventos[i+1:]...)
+			return nil
+		}
+	}
+	return errors.New("evento no encontrado")
+}
+
+//funciones CRUD
+
+// CrearEvento()
+// ObtenerEventos()
+// ObtenerEventoPorID()
+// ActualizarEvento()
+// EliminarEvento()
--- internal/storage/evento_storage.go
+++ internal/storage/evento_storage.go
@@ -1,1 +1,18 @@
 package storage
+
+import "Proyecto_AWEBII/internal/models"
+
+var Eventos []models.Evento // {           //para prueba  =[]
+// 	{
+// 		ID:          1,
+// 		Nombre:      "Congreso Estudiantil",
+// 		Descripcion: "Evento académico",
+// 		Lugar:       "Auditorio",
+// 		Capacidad:   100,
+// 		CategoriaID: 1,
+// 		Organizador: "Asociación",
+// 		Estado:      "Activo",
+// 	},
+// }
+var Categorias []models.CategoriaEvento
+var Asistencias []models.Asistencia
