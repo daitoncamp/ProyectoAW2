f5814287d00ff5d325a69b4a0bb343229998dc64
JS-SeokMin <julissaanchundia9@gmail.com>
2026-06-08 00:22:02 -0500

    Registrar rutas del modulo eventos

--- internal/routes/evento_routes.go
+++ internal/routes/evento_routes.go
@@ -1,1 +1,17 @@
 package routes
+
+//registrar rutas
+
+import (
+	"Proyecto_AWEBII/internal/handlers"
+
+	"github.com/go-chi/chi/v5"
+)
+
+func EventoRoutes(r chi.Router) {
+	r.Get("/events", handlers.ObtenerEventos)
+	r.Post("/events", handlers.CrearEvento)
+	r.Get("/events/{id}", handlers.ObtenerEvento)
+	r.Put("/events/{id}", handlers.ActualizarEvento)
+	r.Delete("/events/{id}", handlers.EliminarEvento)
+}
