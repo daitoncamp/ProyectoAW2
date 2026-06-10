bc52cfb2d062b550e1f6f402a7d23ad862254c31
JS-SeokMin <julissaanchundia9@gmail.com>
2026-06-10 15:33:37 -0500

    Ajustes en el modelo de eventos

--- internal/models/evento.go
+++ internal/models/evento.go
@@ -1,15 +1,13 @@
 package models
 
-import "time"
-
 type Evento struct {
-	ID          int       `json:"id"`
-	Nombre      string    `json:"nombre"`
-	Descripcion string    `json:"descripcion"`
-	Fecha       time.Time `json:"fecha"`
-	Lugar       string    `json:"lugar"`
-	Capacidad   int       `json:"capacidad"`
-	CategoriaID int       `json:"categoria_id"`
-	Organizador string    `json:"organizador"`
-	Estado      string    `json:"estado"`
+	ID          int    `json:"id"`
+	Nombre      string `json:"nombre"`
+	Descripcion string `json:"descripcion"`
+	Fecha       string `json:"fecha"`
+	Lugar       string `json:"lugar"`
+	Capacidad   int    `json:"capacidad"`
+	CategoriaID int    `json:"categoria_id"`
+	Organizador string `json:"organizador"`
+	Estado      string `json:"estado"`
 }
