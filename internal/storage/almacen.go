c4d12e30bc6c4e2f4e46726bab897279fb11b491
JS-SeokMin <julissaanchundia9@gmail.com>
2026-06-21 08:24:18 -0500

    Definir y conectar almacenamiento de eventos con base de datos

--- internal/storage/almacen.go
+++ internal/storage/almacen.go
@@ -2,21 +2,17 @@ package storage
 
 import "Proyecto_AWEBII/internal/models"
 
-type Almacen interface {
-	//Inversiones
-	ListarInversiones() []models.Inversion
-	BuscarInversionPorID(id int) (models.Inversion, bool)
-	CrearInversion(i models.Inversion) models.Inversion
-	ActualizarInversion(id int, datos models.Inversion) (models.Inversion, bool)
-	BorrarInversion(id int) bool
-
-	// EVENTOS
-
+type EventoRepository interface {
 	ListarEventos() []models.Evento
 	BuscarEventoPorID(id int) (models.Evento, bool)
-	CrearEvento(e models.Evento) models.Evento
-	ActualizarEvento(id int, datos models.Evento) (models.Evento, bool)
+	CrearEvento(evento models.Evento) models.Evento
+	ActualizarEvento(id int, evento models.Evento) (models.Evento, bool)
 	BorrarEvento(id int) bool
 }
 
-// var _ Almacen = (*Memoria)(nil)
+type Almacen interface {
+	EventoRepository
+}
+
+var _ Almacen = (*AlmacenSQLite)(nil)
+var _ Almacen = (*Memoria)(nil)
---
+++ internal/storage/sqlite.go
@@ -0,0 +1,66 @@
+package storage
+
+import (
+	"gorm.io/gorm"
+
+	"Proyecto_AWEBII/internal/models"
+)
+
+type AlmacenSQLite struct {
+	db *gorm.DB
+}
+
+func NuevoAlmacenSQLite(db *gorm.DB) *AlmacenSQLite {
+	return &AlmacenSQLite{
+		db: db,
+	}
+}
+
+// Eventos
+
+func (a *AlmacenSQLite) ListarEventos() []models.Evento {
+
+	var eventos []models.Evento
+	a.db.
+		Preload("Categoria").
+		Preload("Asistencias").
+		Find(&eventos)
+	return eventos
+}
+
+func (a *AlmacenSQLite) BuscarEventoPorID(id int) (models.Evento, bool) {
+	var evento models.Evento
+	err := a.db.
+		Preload("Categoria").
+		Preload("Asistencias").
+		First(&evento, id).Error
+	if err != nil {
+		return models.Evento{}, false
+	}
+	return evento, true
+}
+
+func (a *AlmacenSQLite) CrearEvento(e models.Evento) models.Evento {
+	a.db.Create(&e)
+	return e
+}
+
+func (a *AlmacenSQLite) ActualizarEvento(id int, datos models.Evento) (models.Evento, bool) {
+	var existente models.Evento
+	if err := a.db.First(&existente, id).Error; err != nil {
+		return models.Evento{}, false
+	}
+	datos.ID = id
+	a.db.Save(&datos)
+	return datos, true
+}
+
+func (a *AlmacenSQLite) BorrarEvento(id int) bool {
+	res := a.db.Delete(&models.Evento{}, id)
+
+	return res.RowsAffected > 0
+}
+
+// Verificación de interfaz
+
+var _ Almacen = (*AlmacenSQLite)(nil)
